d76db47a833ddfa5e3052a36221334af9924faf3
itang <tang@192.168.0.105>
2018-03-17 11:38:24 +0800

    完善eof

--- chapter5/exercises/eof.cpp
+++ chapter5/exercises/eof.cpp
@@ -6,50 +6,43 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	char input;
-	int count = 0;	
-
-	while(cin >> input)
-	{
-		
-		cout<<input<<flush;
-		count++;
-		
-	}
-	cout<<count<<" End."<<endl;
-	
-	
-	
+void printCinState ()
+{
+	cout << "输入状态如下：" << endl;
 	cout<<"failbit "<<( cin.rdstate() & ios::failbit )<<endl;
 	cout<<"eofbit "<<( cin.rdstate() & ios::eofbit )<<endl;
 	cout<<"badbit "<<( cin.rdstate() & ios::badbit )<<endl;
-	
-	if((cin.fail() && cin.eof()))
+}
+
+int main(int argc, char** argv) {
+	char input;
+	while(cin >> input)
 	{
-		int ch;
+		cout<< "output: "<<input<<'\n';
+	}
+	
+	cout << "输入失败，打印输入流状态：" << endl;
+	printCinState();
+
+	if(cin.eof())
+	{
+		cout << "你结束了输入，导致了输入流失败，并被标记为eof。" << endl;
+		cout << "清除输入流的状态，包括eof，并再次打印状态：" << endl;
 		
 		cin.clear();
 		
-		cout<<"failbit "<<( cin.rdstate() & ios::failbit )<<endl;
-		cout<<"eofbit "<<( cin.rdstate() & ios::eofbit )<<endl;
-		cout<<"badbit "<<( cin.rdstate() & ios::badbit )<<endl;
 		
-		bool bl = !isspace(cin.get());
+
+		cout << "尝试读取一个字符:" << endl;
+		input = cin.get(); // 当get遇到eof的时候会返回结束标记，一般为-1
 		
-		while(bl)
-		{
-			continue;
-		}
+		cout << "打印这个字符和它的编码：" << endl;
+		int code = input;
+		cout << "<" << input << ">" << code << endl;
+
+		printCinState ();
+
 	}
-	else
-	{
-		cout<<"can't go on.\n";
-		exit(1);
-	}
-	
-	while(cin >>input)
-	cout<<input<<flush;
-	
+
 	return 0;
 }
