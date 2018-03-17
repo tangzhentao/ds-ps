eb432ede8fb15e653adbf78ea8365c1f50434631
itang <tang@192.168.0.105>
2018-03-17 22:38:41 +0800

    完善driver

--- chapter5/exercises/5.2-driver.cpp
+++ chapter5/exercises/5.2-driver.cpp
@@ -8,43 +8,21 @@ using namespace std;
 
 int main()
 {
-			char c = cin.get();
-			int ic = c;
-			cout << "[" << c << "]" << ic << endl;
-			return 0;
-
 	string str;
 	string substr; 
 	cout << "Test function: unsigned appearTimes(const string &substr, const string &str);" << endl;
 	for ( ; ; )
 	{
-		cout << "ctrl - d to end" << endl;
+		cout << "q to end" << endl;
 		cout << "Please type a string: " << endl;
+
 		cin >> str;
+		// 判断是不是要结束测试
+		if ("q" == str)
+			break;
+
 		cout << "Please type a substring: " << endl;
 		cin >> substr;
-
-		if (cin.eof())
-		{
-			cin.clear();
-			cin.sync();
-			if (cin.good())
-				cout << "*** cin good ***" << endl;
-
-			if (cin.fail())
-				cout << "*** cin fail ***" << endl;
-
-			if (cin.eof())
-				cout << "*** cin eof" << endl;
-
-			cin.ignore();
-			char c = cin.get();
-			int ic = c;
-			cout << "[" << c << "]" << ic << endl;
-			return 0;
-
-			break;
-		}
 
 		int times = appearTimes(substr, str);
 		cout << substr << '\n'
@@ -68,13 +46,13 @@ int main()
 		cout << "The " << monthNumber << " month is " << name << endl;
 	}
 
-	cout << "\nTest function: nsigned monthNum(const string &month);" << endl;
+	cout << "\nTest function: unsigned monthNum(const string &month);" << endl;
 	string monthName = "";
 	for ( ; ; )
 	{
-		cout << "Type the name of month(e to end): ";
+		cout << "Type the name of month(q to end): ";
 		cin >> monthName;
-		if ("e" == monthName)
+		if ("q" == monthName)
 			break;
 
 		unsigned num = monthNum(monthName);
