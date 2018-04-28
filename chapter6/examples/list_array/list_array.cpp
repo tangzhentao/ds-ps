853876b3c76798911b3b221e8a06a9503e28be28
tang <itangzhentao@126.com>
2018-04-28 17:53:33 +0800

    list_array

--- chapter6/examples/list_array/list_array.cpp
+++ chapter6/examples/list_array/list_array.cpp
@@ -11,22 +11,89 @@ List::List()
 	size = 0;
 }
 
-bool List::isEmpty()
+List::List(ElementType a[], int theSize)
+{
+	cout << __func__ << endl;
+	size = theSize;
+	for (int i = 0; i < theSize; i++)
+	{
+		array[i] = a[i];
+	}
+	cout << "List end" << endl;
+}
+
+bool List::isEmpty() const
 {
 	return (0 == size);
 }
 
-void List::insert(int item, unsigned pos)
+void List::insert(ElementType item, int pos)
 {
 	/*
 	 * 1、先判断还有没有容量
 	 * 2、再判断pos是否合法
 	 * 3、移动相关元素
 	 */
+	cout << "insert " << item << " at " << pos << endl;
 
 	if (size == CAPACITY)
 		return ;
 
-	if (pos > size)
+	if (pos > size || pos < 0)
 		return ;
+
+	// 首先右移数组元素为item腾出空间
+	for (int i = size; i > pos; i--)
+	{
+		array[i] = array[i - 1];
+	}
+
+	// pos以及之后的元素移动完成，此时插入新元素
+	array[pos] = item;
+
+	// 列表大小加一
+	++size;
+}
+
+void List::erase(int pos)
+{
+	cout << "erase " << pos << endl;
+	if (isEmpty())
+		return;
+	
+	// 判断位置是否合法
+	if (pos < 0 || pos > size - 1)
+		return ;
+
+	// 把要删除的位置置0
+	array[pos] = 0;
+
+	// 
+	for (int i = pos; i < size - 1; i++)
+	{
+		array[i] = array[i+1];
+	}
+
+	// 减少size
+	--size;
+}
+
+void List::display(ostream &out) const
+{
+	out << "List: " << endl;
+	for (int i = 0; i < size; i++)
+	{
+		out << array[i] << "  ";
+	}
+	out << endl;
+}
+
+ostream & operator << (ostream &out, const List &list)
+{
+	if (list.isEmpty())
+		return out;
+
+	list.display(out);
+
+	return out;
 }
--- chapter6/examples/list_array/list_array.h
+++ chapter6/examples/list_array/list_array.h
@@ -9,13 +9,18 @@
  */
 #include <iostream>
 
-#define CAPACITY 100
+#ifndef LIST
+#define LIST
+
+typedef int ElementType;
+const int CAPACITY = 1024;
 
 using namespace std;
 
 class List
 {
 public:
+
 	/*
 	 * 默认构造函数
 	 *
@@ -28,9 +33,20 @@ public:
 	List();
 
 	/*
+	 * 自定义构造函数
+	 *
+	 * 前置条件：
+	 *	提供一个数组
+	 *
+	 * 后置条件：
+	 *	创建一个元素为给定数组的列表
+	 */
+	List(ElementType a[], int size);
+
+	/*
 	 * 判断是不是空链表
 	 */
-	bool isEmpty();
+	bool isEmpty() const;
 
 	/*
 	 * 在指定位置插入一个元素
@@ -39,7 +55,7 @@ public:
 	 * 后置条件：
 	 *  item被插入到指定的位置
 	 */
-	void insert(int item, unsigned pos);
+	void insert(ElementType item, int pos);
 	
 	/*
 	 * 删除给定的元素
@@ -48,7 +64,7 @@ public:
 	 * 后置条件：
 	 *  返回有没删除成功？返回失败原因？
 	 */
-	void erase(int item);
+	void erase(int pos);
 
 	/*
 	 * 遍历列表
@@ -58,9 +74,19 @@ public:
 	 * 后置条件：
 	 *  打印列表的所有元素
 	 */
-	void traverse();
+	void display(ostream &out) const;
+
 
 private:
-	int array[CAPACITY];
+	ElementType array[CAPACITY];
 	int size;
 };
+
+/*
+ * 重载输出操作符
+ *
+ * 前置条件：输出流已经打开
+ */
+ostream & operator << (ostream &out, const List &list);
+
+#endif
